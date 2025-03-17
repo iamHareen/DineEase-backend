666ad1032ac924e0912f8f54c3c4bd79059400f6
iamHareen <helanm06@gmail.com>
2025-03-17 14:55:10 +0530

    first commit

---
+++ src/main/java/com/hareendev/dineease/DineeaseApplication.java
@@ -0,0 +1,13 @@
+package com.hareendev.dineease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hareendev/dineease/DineeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hareendev.dineease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
