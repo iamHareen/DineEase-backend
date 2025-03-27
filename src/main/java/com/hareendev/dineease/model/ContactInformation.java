49222f4e4bd473f155234d68424acf5f00d8d788
iamHareen <helanm06@gmail.com>
2025-03-28 03:11:28 +0530

    update ContactInformation model

--- src/main/java/com/hareendev/dineease/model/ContactInformation.java
+++ src/main/java/com/hareendev/dineease/model/ContactInformation.java
@@ -1,10 +1,6 @@
 package com.hareendev.dineease.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Data
 public class ContactInformation {
