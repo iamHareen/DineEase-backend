1c87b561a0b52902c160ba7f65e78db2edded86e
iamHareen <helanm06@gmail.com>
2025-03-28 03:04:59 +0530

    add initial model files

---
+++ src/main/java/com/hareendev/dineease/model/CartItem.java
@@ -0,0 +1,31 @@
+package com.hareendev.dineease.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Cart cart;
+
+    private int quantity;
+
+    private List<String> ingredients;
+
+    private Long totalPrice;
+
+}
+
