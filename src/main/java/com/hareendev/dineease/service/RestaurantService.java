cfd69b75a9108087007f00d9d7f24918b2c6028f
iamHareen <helanm06@gmail.com>
2025-11-15 08:52:15 +0530

    restaurant service implemented

---
+++ src/main/java/com/hareendev/dineease/service/RestaurantService.java
@@ -0,0 +1,29 @@
+package com.hareendev.dineease.service;
+
+import com.hareendev.dineease.dto.RestaurantDTO;
+import com.hareendev.dineease.model.Restaurant;
+import com.hareendev.dineease.model.User;
+import com.hareendev.dineease.request.CreateRestaurantRequest;
+
+import java.util.List;
+
+public interface RestaurantService {
+
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user);
+
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updateRestaurant) throws Exception;
+
+    public void deleteRestaurant(Long restaurantId) throws Exception;
+
+    public List<Restaurant> getAllRestaurant();
+
+    public List<Restaurant> searchRestaurant();
+
+    public Restaurant findRestaurantById(Long id) throws Exception;
+
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception;
+
+    public Restaurant addToFavourites(Long restaurantId, User user) throws Exception;
+
+    public RestaurantDTO updateRestaurantStatus(Long id) throws Exception;
+}
---
+++ src/main/java/com/hareendev/dineease/service/RestaurantServiceImp.java
@@ -0,0 +1,70 @@
+package com.hareendev.dineease.service;
+
+import com.hareendev.dineease.dto.RestaurantDTO;
+import com.hareendev.dineease.model.Restaurant;
+import com.hareendev.dineease.model.User;
+import com.hareendev.dineease.repository.AddressRepository;
+import com.hareendev.dineease.repository.RestaurantRepository;
+import com.hareendev.dineease.request.CreateRestaurantRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantServiceImp implements RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user) {
+        return null;
+    }
+
+    @Override
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updateRestaurant) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteRestaurant(Long restaurantId) throws Exception {
+
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurant() {
+        return List.of();
+    }
+
+    @Override
+    public List<Restaurant> searchRestaurant() {
+        return List.of();
+    }
+
+    @Override
+    public Restaurant findRestaurantById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Restaurant addToFavourites(Long restaurantId, User user) throws Exception {
+        return null;
+    }
+
+    @Override
+    public RestaurantDTO updateRestaurantStatus(Long id) throws Exception {
+        return null;
+    }
+}
